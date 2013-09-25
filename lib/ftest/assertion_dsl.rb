7827b466fcd633972c8b7c7a924da71d6f5525d6
Christian Marie <pingu@anchor.net.au>
2013-09-25 12:45:25 +1000

    Wat.

--- lib/ftest/assertion_dsl.rb
+++ lib/ftest/assertion_dsl.rb
@@ -23,17 +23,12 @@ module FTest::ScenarioDSL::AssertionDSL
 	end
 
 	# Check that a.include? b
-	def assert_inclusion(a, b, description='')
+	def assert_inclusion(a, b, description='inclusion assertion failed')
 		return if a.include?(b) 
-		
-		begin
-			failure = ::FTest::Failure.new(description)
-			failure.extra_details << "assert_inclusion failure: "\
-				"#{a.inspect}.include? #{b.inspect} == false"
-			raise failure
-		rescue
-			# Just in case
-			raise ::FTest::Failure.new(description)
-		end
+	
+		failure = ::FTest::Failure.new(description)
+		failure.extra_details << "assert_inclusion failure: "\
+			"#{a.inspect}.include? #{b.inspect} == false"
+		raise failure
 	end
 end
